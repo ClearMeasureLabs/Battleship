730db100f8af6103e1895fbb5ecba2f76b189ad4
Jeffrey Palermo <jeffrey@clear-measure.com>
2018-05-21 09:31:45 -0500

    - need to autoinstall VSSetup

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { Install-Module VSSetup -Scope CurrentUser }"
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { Install-Module VSSetup -Scope CurrentUser -Force; if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED; exit $lastexitcode}  }"
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { Import-Module '.\src\packages\psake.4.7.0\tools\psake\psake.psm1'; Invoke-psake '.\default.ps1' %1; if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED; exit $lastexitcode} }" 
 
